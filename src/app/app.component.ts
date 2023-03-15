afed6bee01130b174c2e6095714cc662a2bcc23a
Harshali15 <harshalijain.1504@gmail.com>
2023-03-15 16:09:45 -0700

    temaplates and directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'InventoryMangementApp';
+  
+  //role= 'User'
+  role= 'Admin'
+
 }
---
+++ src/app/rooms/rooms.ts
@@ -0,0 +1,16 @@
+export interface Rooms{
+    totalRoom: number;
+    availableRooms: number;
+    bookedRooms: number;
+    onholdRooms?: number;
+}
+
+export interface RoomsList{
+    roomNumber: number;
+    roomType: string;
+    ammenities: string;
+    price: number;
+    image: string;
+    checkinTime: Date;
+    checkoutTime: Date;
+}
