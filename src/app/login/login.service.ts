f6c700e38251de98d6b4b6e60b8064077d93c926
Harshali15 <harshalijain.1504@gmail.com>
2023-05-04 23:39:36 -0700

    canDeactivate guard

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -7,11 +7,18 @@ export class LoginService {
 
   isLoggedin: boolean = false;
 
+  isAdminLogin: boolean = false;
+
   constructor() { }
 
   login(email:string, password:string) {
     if(email=="admin@gmail.com" && password=="admin"){
         this.isLoggedin=true;
+        this.isAdminLogin=true;
+    }
+    if(email=="user@gmail.com" && password=="user"){
+      this.isLoggedin=true;
+      this.isAdminLogin=false;
     }
     return this.isLoggedin;
   }
--- src/app/rooms/rooms-routing.module.ts
+++ src/app/rooms/rooms-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { RoomsAddComponent } from './rooms-add/rooms-add.component';
 import { RoomsBookingComponent } from './rooms-booking/rooms-booking.component';
 import { RoomsComponent } from './rooms.component';
+import { LoginGuard } from '../guards/login.guard';
+import { RoomGuard } from './guards/room.guard';
 
 const routes: Routes = [
+  { path: 'rooms/add', component: RoomsBookingComponent},
   { path: '', component: RoomsComponent,
+    canActivateChild: [RoomGuard],
     children: [{path: ':id', component: RoomsBookingComponent}],
   },
   // { path: ':id', component: RoomsAddComponent},
-  { path: 'rooms/add', component: RoomsBookingComponent},
+  
 ];
 
 @NgModule({
