f4fcdcc6bf45df488430d3a51c65e5d5f9f08666
Harshali15 <harshalijain.1504@gmail.com>
2023-05-05 00:07:21 -0700

    Reactive forms

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ConfigserviceService } from '../services/configservice.service';
+import { FormGroup, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.scss']
 })
-export class BookingComponent {
-constructor(private configService: ConfigserviceService) { }
+export class BookingComponent implements OnInit{
+
+  bookingForm!: FormGroup
+  constructor(private configService: ConfigserviceService,
+    private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.bookingForm = this.fb.group({ 
+        roomId: [''], //[''] is a shortcut to new FormControl('')
+        guestEmail: [''],
+        checkinDate: [''],
+        checkoutDate: [''],
+        bookingstatus: [''],
+        bookingAmount: [''],
+        bookingDate:[''],
+        mobileNumber: [''],
+        guestName: [''],
+        guestAddress: [''],
+        guestCity: [''],
+        guestState: [''],
+        guestCountry: [''],
+        guestZipCode: [''],
+        guestCount: ['']    
+      })
+  }
+
 }
+
+
+// export class Booking {
+//   roomId: string;
+//   guestEmail: string;
+//   checkinDate: Date;
+//   checkoutDate: Date;
+//   •bookingstatus: string;
+//   bookingAmount: number;
+//   bookingDate: Date;
+//   mobileNumber: string;
+//   guestName: string;
+//   guestAddress: string;
+//   guestCity: string;
+//   guestState: string;
+//   guestCountry: string;
+//   guestZipCode: string;
+//   guestCount: number;
+//   guestList: [];
+// }
